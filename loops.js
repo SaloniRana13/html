b065bce04d66ed93eb4b8512aea89032ef7ce2c2
SaloniRana13 <salonirana1342001@gmail.com>
2025-07-12 18:18:46 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,19 @@
+ for (let i = 0; i < 5; i++) {
+  console.log("For loop, iteration:", i);
+}
+
+ 
+ 
+ 
+//  let i = 0;
+// while (i < 5) {
+//   console.log("While loop, iteration:", i);
+//   i++;
+// }
+
+
+// let i = 0;
+// do {
+//   console.log("Do-While loop, iteration:", i);
+//   i++;
+// } while (i < 5);
