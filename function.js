b307f343ae81303c8016aca7df81c247aa3ec717
SaloniRana13 <salonirana1342001@gmail.com>
2025-07-13 19:04:25 +0530

    Add files via upload

--- function.js
+++ function.js
@@ -14,3 +14,13 @@ console.log(greet("Alice")); // Output: Hello, Alice!
 const add = function(a, b) {
   return a + b;
 };
+
+
+
+
+function greet(name = "Guest") {
+  console.log("Hello, " + name + "!");
+}
+
+greet(); // "Hello, Guest!"
+greet("Charlie"); // "Hello, Charlie!"
