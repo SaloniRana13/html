a7a9cb5cc9ebbad5e3e9ba4a2c76ca67cda9f611
SaloniRana13 <salonirana1342001@gmail.com>
2025-07-28 11:48:32 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,32 @@
+// What are the different data types in JavaScript?
+
+// Explain var, let, and const.
+
+// What is hoisting?
+
+// What is the difference between == and ===?
+
+// What are truthy and falsy values?
+
+// What is a closure?
+
+// Explain scope in JavaScript (global, function, block).
+
+
+reverseString("hello") // Output: "olleh"
+
+isPalindrome("madam") // Output: true
+
+
+
+findMax([1, 3, 9, 2]) // Output: 9
+
+
+flatten([1, [2, [3, 4], 5]]) // Output: [1, 2, 3, 4, 5]
+
+
+removeDuplicates([1, 2, 2, 3, 4, 4]) // Output: [1, 2, 3, 4]
+
+const obj = { a: 1, b: { c: 2 } }
+deepClone(obj) // Should deeply clone nested objects
+
