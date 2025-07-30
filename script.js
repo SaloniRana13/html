d9f0b79d3d104b27ca7283b59a1bbc67fec81cad
SaloniRana13 <salonirana1342001@gmail.com>
2025-07-30 20:53:43 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
+const hamburger = document.getElementById('hamburger');
+    const navLinks = document.getElementById('navLinks');
 
-const apiUrl = 'https://jsonplaceholder.typicode.com/posts/1';
-
-async function fetchData() {
-  try {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-  
-    document.getElementById('output').textContent = JSON.stringify(data, null, 2);
-  } catch (error) {
-    document.getElementById('output').textContent = 'Error loading data.';
-    console.error('Fetch error:', error);
-  }
-}
-
-fetchData();
+    hamburger.addEventListener('click', () => {
+      navLinks.classList.toggle('show');
+       });
