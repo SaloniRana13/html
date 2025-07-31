94538da65ec845d2449eaab4b16511beb69f8086
SaloniRana13 <salonirana1342001@gmail.com>
2025-07-31 18:23:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ function sum(...numbers) {
 }
 
 console.log(sum(1, 2, 3, 4)); // 10
+
+
+const [first, second, ...rest] = [10, 20, 30, 40, 50];
+console.log(first);  // 10
+console.log(second); // 20
+console.log(rest);   // [30, 40, 50]
+
+
+
+const user = {
+  name: "Alice",
+  age: 25,
+  country: "USA",
+  role: "admin"
+};
+
+const { name, ...others } = user;
+console.log(name);   // Alice
+console.log(others); // { age: 25, country: "USA", role: "admin" }
