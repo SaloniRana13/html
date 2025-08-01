c758c6e85d4afffdca8495c0ded4e0f7f158502a
SaloniRana13 <salonirana1342001@gmail.com>
2025-08-01 20:33:48 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,59 @@
-function sum(...numbers) {
-  return numbers.reduce((acc, curr) => acc + curr, 0);
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
+
+let transactions = [];
+
+function addTransaction(e) {
+    e.preventDefault();
+
+    if (text.value.trim() === '' || amount.value.trim() === '') {
+        alert('Please add a text and amount');
+        return;
+    }
+
+    const transaction = {
+        id: Date.now(),
+        text: text.value,
+        amount: +amount.value
+    };
+
+    transactions.push(transaction);
+    addTransactionDOM(transaction);
+    updateValues();
+    text.value = '';
+    amount.value = '';
 }
 
-console.log(sum(1, 2, 3, 4)); // 10
+function addTransactionDOM(transaction) {
+    const sign = transaction.amount < 0 ? '-' : '+';
 
+    const item = document.createElement('li');
+    item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
 
-const [first, second, ...rest] = [10, 20, 30, 40, 50];
-console.log(first);  // 10
-console.log(second); // 20
-console.log(rest);   // [30, 40, 50]
+    item.innerHTML = `
+        ${transaction.text} <span>${sign}$${Math.abs(transaction.amount)}</span>
+    `;
 
+    list.appendChild(item);
+}
 
+function updateValues() {
+    const amounts = transactions.map(t => t.amount);
 
-const user = {
-  name: "Alice",
-  age: 25,
-  country: "USA",
-  role: "admin"
-};
+    const total = amounts.reduce((acc, item) => acc + item, 0).toFixed(2);
+    const income = amounts.filter(i => i > 0).reduce((acc, item) => acc + item, 0).toFixed(2);
+    const expense = (
+        amounts.filter(i => i < 0).reduce((acc, item) => acc + item, 0) * -1
+    ).toFixed(2);
 
-const { name, ...others } = user;
-console.log(name);   // Alice
-console.log(others); // { age: 25, country: "USA", role: "admin" }
+    balance.innerText = `$${total}`;
+    money_plus.innerText = `+$${income}`;
+    money_minus.innerText = `-$${expense}`;
+}
+
+form.addEventListener('submit', addTransaction);
