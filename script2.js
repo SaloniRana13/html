98d28cca31bcbf680791123e12dc909e5c24f227
SaloniRana13 <salonirana1342001@gmail.com>
2025-08-05 22:29:23 +0530

    Add files via upload

--- script2.js
+++ script2.js
@@ -1,25 +1,8 @@
-// const list = document.getElementById("myList");
-// const fragment = document.createDocumentFragment();
-
-// for (let i = 0; i < 1000; i++) {
-//   const li = document.createElement("li");
-//   li.textContent = `Item ${i}`;
-//   fragment.appendChild(li);
-// }
-
-// list.appendChild(fragment); // 1 reflow instead of 1000
-
-
-
-function createCard(title, content) {
-  const card = document.createElement("div");
-  card.className = "card";
-
-  card.innerHTML = `
-    <h3>${title}</h3>
-    <p>${content}</p>
-  `;
-  return card;
-}
-
-document.body.appendChild(createCard("JS Tips", "Use fragments for performance."));
+const handler = {
+  get(target, prop) {
+    return prop in target ? target[prop] : "Not found";
+  }
+};
+const proxy = new Proxy({ name: "Chavi" }, handler);
+console.log(proxy.name);  // Chavi
+console.log(proxy.age);   // Not found
