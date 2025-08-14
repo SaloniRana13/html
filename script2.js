630ae6571e6139a190edacdb229ce9970b2648e6
SaloniRana13 <salonirana1342001@gmail.com>
2025-08-14 20:09:43 +0530

    Add files via upload

--- script2.js
+++
@@ -1,8 +0,0 @@
-const handler = {
-  get(target, prop) {
-    return prop in target ? target[prop] : "Not found";
-  }
-};
-const proxy = new Proxy({ name: "Chavi" }, handler);
-console.log(proxy.name);  // Chavi
-console.log(proxy.age);   // Not found
