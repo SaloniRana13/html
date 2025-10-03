7e384a9b1f707b46524d2ed9d1d56697a77df6b6
SaloniRana13 <salonirana1342001@gmail.com>
2025-10-03 15:46:03 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,44 @@
+// let color ="red";
+// if(color="red"){
+//     console.log("stop");
+// }
+   
+// if(color=="yellow"){
+//     console.log("slow down");
+// }
+// if(color=="green"){
+//     console.log("ready to go");
+// }
+// let size ="S";
+// if(size=="XL"){
+//     console.log("price is Rs. 250");
+// }else if(size=="L"){
+//     console.log("price is Rs. 200");
+// }else if(size=="M"){
+//     console.log("price is Rs. 100");
+// } else{
+//     console.log("price is Rs. 50");
+// }
+let day=1;
+switch(day){
+    case 1:console.log("monday");
+    break;
+    case 2:console.log("tuesday");
+    break;
+    case 3:console.log("wednesday");
+    break;
+    case 4:console.log("thursday");
+    break;
+    case 5:console.log("friday");
+    break;
+    case 6:console.log("saturday");
+    break;
+    case 7:console.log("sunday");
+    break;
+    default:console.log("invalid input");
+}
+ let y=prompt("enter ur name :");
+ let z=prompt("enter ur last name:");
+ let w=y+" "+z;
+ console.log(w);
+ alert("someyhing went wrong");
