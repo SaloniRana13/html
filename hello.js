af6d4bc2064c6b06a7b9056d4f48a01080f73a95
SaloniRana13 <salonirana1342001@gmail.com>
2025-10-26 12:20:31 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,12 @@
+let a="sanu";
+const b=111;
+console.log(a);
+console.log(b);
+let pen=10;
+let blue=5;
+let white=5;
+// let output="total price :"+(pen+blue+white)+"ruppes.";
+ let op=`the total price: ${pen+blue+white} ruppes`;
+//console.log("total price :",pen+blue+white,"rupee");
+console.log(op)
+console.log(blue!=white);
