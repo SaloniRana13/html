6739bfff8d44f6c6c228d118e5ce67b6cfe4f935
SaloniRana13 <salonirana1342001@gmail.com>
2025-11-18 19:16:27 +0530

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,22 +1,10 @@
-// src/main.jsx
-import React from 'react';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import { Provider } from 'react-redux';
-import store from './redux/Store';
-
-// ✅ Unified Mirage server
-import { makeServer } from './mirage/server';
-
-if (process.env.NODE_ENV === 'development') {
-  makeServer();
-}
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </Provider>
-);
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
