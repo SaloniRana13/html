67958e0a9ab546fd34c69a7113cfd54995a1ac31
SaloniRana13 <salonirana1342001@gmail.com>
2026-01-15 20:43:40 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,46 @@
+ let arr=[6,9,8,8,8,5];//arr.length==6
+ let count=0;
+ for (let i=0;i<arr.length;i++){//6<6 flase
+
+    if(arr[i]<=8 ){
+    print ("lesser than 8",arr[i])
+    }
+ }
+ //count==6
+ count=== 1
+[1,3,5,7,8]
+ return count;
+ //arr.length==6,   first ==0
+ for(let i=arr.lenth-1;i>=0;i--){//0==0====
+    console.log(arr[i]);//5
+   5 4 3 2 1 0 -1
+ }
+
+
+
+ [3,5,7,8,4] //target=12
+
+ 3+5
+ 3+7
+ 3+8
+ 3+4
+5=i; j=3,7,8,4
+7=i;j=8,4
+
+let arr1 = [1,2,3,4];
+let arr2 = [4,5,6];
+let newArr = [];
+
+function mergeArray() {
+    for (let i = 0; i < arr1.length; i++) {
+        newArr=arr1[i];
+    }
+
+    for (let i = 0; i < arr2.length; i++) {
+        newArr=arr2[i];
+    }
+
+    return newArr;
+}
+
+console.log(mergeArray());
